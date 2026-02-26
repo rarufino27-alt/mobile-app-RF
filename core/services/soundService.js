c5d886e173a19d5970c043b368a728ee58ff773c
rarufino27-alt <rarufino27@gmail.com>
2026-02-26 12:09:03 -0300

    Atualizar o soundService.js

--- core/services/soundService.js
+++ core/services/soundService.js
@@ -1,25 +1,32 @@
 window.SoundService = {
 
   sounds: {
-    SEM_CREDITO: "https://rarufino27-alt.github.io/mobile-app-RF/assets/sounds/sem-credito.mp3",
-    CHAMADA: "https://rarufino27-alt.github.io/mobile-app-RF/assets/sounds/notificacao.mp3",
-    ACEITA: "https://rarufino27-alt.github.io/mobile-app-RF/assets/sounds/viagem-aceita.mp3",
-    CANCELADA: "https://rarufino27-alt.github.io/mobile-app-RF/assets/sounds/viagem-cancelada.mp3",
-    CREDITO: "https://rarufino27-alt.github.io/mobile-app-RF/assets/sounds/credito-adicionado.mp3",
-    MSG_PASSAGEIRO: "https://rarufino27-alt.github.io/mobile-app-RF/assets/sounds/mensagem.mp3",
-    MSG_APP: "https://rarufino27-alt.github.io/mobile-app-RF/assets/sounds/mensagem1.mp3",
-    ONLINE: "https://rarufino27-alt.github.io/mobile-app-RF/assets/sounds/online.mp3",
-    OFFLINE: "https://rarufino27-alt.github.io/mobile-app-RF/assets/sounds/viagem-cancelada.mp3"
+    SEM_CREDITO: "../assets/sounds/sem-credito.mp3",
+    CHAMADA: "../assets/sounds/notificacao.mp3",
+    ACEITA: "../assets/sounds/viagem-aceita.mp3",
+    CANCELADA: "../assets/sounds/viagem-cancelada (1).mp3",
+    CREDITO: "../assets/sounds/credito-adicionado.mp3",
+    MSG_PASSAGEIRO: "../assets/sounds/mensagem.mp3",
+    MSG_APP: "../assets/sounds/mensagem1.mp3",
+    ONLINE: "../assets/sounds/online.mp3",
+    OFFLINE: "../assets/sounds/viagem-cancelada (1).mp3"
   },
 
   play(type){
 
-    if(!this.sounds[type]) return;
+    const src = this.sounds[type];
 
-    const audio = new Audio(this.sounds[type]);
+    if(!src){
+      console.warn("Som não encontrado:", type);
+      return;
+    }
+
+    const audio = new Audio(src);
     audio.volume = 1;
-    audio.play().catch(()=>{});
 
+    audio.play().catch(err=>{
+      console.log("Autoplay bloqueado:", err);
+    });
   }
 
 };
